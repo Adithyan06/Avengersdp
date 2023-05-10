c3c0a80644a7459605222a0886717d8ad5e543a3
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-10 21:28:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,9 @@ from pyrogram import Client, filters
 
 from pyrogram.types import Message
 
-import spotipy
-
-from spotipy.oauth2 import SpotifyClientCredentials
-
-import requests
-
-# Set up your Spotify credentials
-
-client_id = os.environ["Client_ID"]
-client_secret = os.environ["Client_secret"]
+from jiosaavnapi import JioSaavn
+# client_id = os.environ["Client_ID"]
+# client_secret = os.environ["Client_secret"]
 
 # Set up your Telegram bot token
 
@@ -33,93 +26,51 @@ app = Client(
 
 # Set up Spotify client
 
-spotify_client = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
-
-# Define a function to search for a song on Spotify
-
-def search_song(query: str):
-
-    results = spotify_client.search(query, limit=1, type='track')
-
-    if results['tracks']['items']:
-
-        return results['tracks']['items'][0]
-
-    return None
-
-# Define a function to download the song from Spotify
-
-def download_song(track_id: str):
-
-    track_info = spotify_client.track(track_id)
-
-    audio_file = track_info['preview_url']
-
-    if audio_file:
-
-        response = requests.get(audio_file)
-
-        return response.content
-
-    return None
-
-# Define a function to handle incoming messages
+jiosaavn = JioSaavn()
+# Function to handle incoming messages
 
 @app.on_message(filters.command("start"))
 
 def handle_start_command(client: Client, message: Message):
 
-    welcome_message = "Welcome to the Song Downloader Bot! Send me the name of a song, and I'll try to find and download it for you."
+    welcome_message = "Welcome to JioSaavn Bot! Send me the name of a song to download."
 
     client.send_message(chat_id=message.chat.id, text=welcome_message)
 
 @app.on_message(filters.text)
 
-def handle_message(client: Client, message: Message):
+def handle_song_download(client: Client, message: Message):
 
-    # Get the user's message
+    # Get the song name from the user's message
 
-    user_message = message.text
+    song_name = message.text.strip()
 
-    # Search for the song on Spotify
+    # Search for the song on JioSaavn
 
-    track = search_song(user_message)
+    results = jiosaavn.search_for_song(song_name)
 
-    if track:
+    # Check if any results found
 
-        # Download the song from Spotify
+    if results:
 
-        audio_data = download_song(track['id'])
+        # Get the first result
 
-        if audio_data:
+        song = results[0]
 
-            # Save the song as an audio file
+        # Download the song
 
-            file_path = f"{track['name']} - {track['artists'][0]['name']}.mp3"
+        song_path = song.download()
 
-            with open(file_path, 'wb') as audio_file:
+        # Send the downloaded song to the user
 
-                audio_file.write(audio_data)
-
-            # Send the song file to the user
-
-            client.send_audio(chat_id=message.chat.id, audio=file_path)
-
-            os.remove(file_path)  # Remove the temporary audio file
-
-        else:
-
-            error_message = "Sorry, I couldn't download the song. Please try again later."
-
-            client.send_message(chat_id=message.chat.id, text=error_message)
+        client.send_audio(chat_id=message.chat.id, audio=song_path, title=song.title, performer=song.artist)
 
     else:
 
-        not_found_message = "Sorry, I couldn't find the song on Spotify. Please try a different song."
+        error_message = "Sorry, I couldn't find the song you requested."
 
-        client.send_message(chat_id=message.chat.id, text=not_found_message)
+        client.send_message(chat_id=message.chat.id, text=error_message)
 
 # Run the Pyrogram client
 
 app.run()
-
