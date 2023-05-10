510d992aab96a5adf3f216479660c12e4ec7a987
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-10 18:43:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import os
 
+import asyncio
+
 import pyrogram
 
 from pyrogram import Client, filters
 
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, InlineQuery, InlineQueryResultArticle, \
+
+    InputTextMessageContent
 
 import openai
 
 # Set up your OpenAI credentials
-
 openai.api_key = os.environ["openai.api_key"]
 
 # Set up your Telegram bot token
+
 app = Client(
 
     "Logo Bot",
@@ -35,7 +39,7 @@ def generate_response(message: str) -> str:
 
     response = openai.Completion.create(
 
-        engine="text-davinci-002",  # or "text-davinci-002" for GPT-3 (non-turbo)
+        engine="text-davinci-003",  # or "text-davinci-002" for GPT-3 (non-turbo)
 
         prompt=message,
 
@@ -55,15 +59,15 @@ def generate_response(message: str) -> str:
 
 @app.on_message(filters.command("start"))
 
-def handle_start_command(client: Client, message: Message):
+async def handle_start_command(client: Client, message: Message):
 
     welcome_message = "Welcome to the ChatBot! Send me a message, and I'll respond."
 
-    client.send_message(chat_id=message.chat.id, text=welcome_message)
+    await message.reply_text(welcome_message)
 
-@app.on_message(~filters.command("start"))
+@app.on_message(filters.text)
 
-def handle_message(client: Client, message: Message):
+async def handle_text_message(client: Client, message: Message):
 
     # Get the user's message
 
@@ -75,9 +79,91 @@ def handle_message(client: Client, message: Message):
 
     # Send the response back to the user
 
-    client.send_message(chat_id=message.chat.id, text=response)
+    await message.reply_text(response)
 
-# Run the Pyrogram client
+@app.on_message(filters.photo)
 
-app.run()
+async def handle_photo_message(client: Client, message: Message):
+
+    # Get the photo file ID
+
+    photo_file_id = message.photo[-1].file_id
+
+    # Download the photo file
+
+    photo_path = await client.download_media(photo_file_id)
+
+    # Process the photo (if needed)
+
+    # ...
+
+    # Generate a response using OpenAI
+
+    response = generate_response("I received a photo!")
+
+    # Send the response back to the user
+
+    await message.reply_text(response)
+
+@app.on_message(filters.document)
+
+async def handle_document_message(client: Client, message: Message):
+
+    # Get the document file ID
+
+    document_file_id = message.document.file_id
+
+    # Download the document file
+
+    document_path = await client.download_media(document_file_id)
+
+    # Process the document (if needed)
+
+    # ...
+
+    # Generate a response using OpenAI
+
+    response = generate_response("I received a document!")
+
+    # Send the response back to the user
+
+    await message.reply_text(response)
+
+@app.on_message(filters.command("interactive"))
+
+async def handle_interactive_command(client: Client, message: Message):
+
+    # Generate an interactive response using OpenAI
+
+    response = generate_response("What is your favorite color?")
+
+    # Create an inline keyboard with color options
+
+    keyboard = InlineKeyboardMarkup(
+
+        [
+
+            [
+
+                InlineKeyboardButton("Red", callback_data="red"),
+
+                InlineKeyboardButton("Blue", callback_data="blue"),
+
+            ],
+
+            [
+
+                InlineKeyboardButton("Green", callback_data="green"),
+
+                InlineKeyboardButton("Yellow", callback_data="yellow"),
+
+            ],
+
+        ]
+
+    )
+
+    # Send the interactive response back to the user
+
+    await message.reply_text(response, reply_markup=keyboard)
 
