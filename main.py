97e585a26b04a12aa248e5e3105e59f21c222ec3
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-11 10:21:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def handle_logo_creation(client: Client, message: Message):
 
     # Create the logo using LogoMaker
 
-    logo_path = logomaker.create_logo(text)
+    logo_path = logomaker.demo(text)
 
     # Send the logo to the user
 
