1c183f6bd909a96eb993436665368b1cbd33abb3
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2023-05-16 17:54:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+
 from pyrogram import Client, filters
+
 from pyrogram.types import Message
+
 
 app = Client(
 
@@ -13,177 +16,78 @@ app = Client(
     api_hash = os.environ["API_HASH"]
 
 )
-# Authorized users (add your desired user IDs here)
 
-# authorized_users = [USER_ID_1, USER_ID_2, USER_ID_3]
-
-
-users = set()
-
-def get_text(message: Message) -> [None, str]:
-
-    """Extract Text From Commands"""
-
-    text_to_return = message.text
-
-    if message.text is None:
-
-        return None
-
-    if " " in text_to_return:
-
-        try:
-
-            return message.text.split(None, 1)[1]
-
-        except IndexError:
-
-            return None
-
-    else:
-
-        return None
-
-# Function to handle incoming messages
+# Handler for /start command
 
 @app.on_message(filters.command("start"))
 
-def handle_start_command(client: Client, message: Message):
+def start_handler(client: Client, message: Message):
 
-    welcome_message = "Hello 💦"
+    client.send_message(
 
-    client.send_message(chat_id=message.chat.id, text=welcome_message)
-    users.add(message.chat.id)
+        chat_id=message.chat.id,
 
-# Function to handle /broadcast command
+        text="Welcome! Please send me a video file to compress."
 
-@app.on_message(filters.command("ad"))
+    )
 
-def handle_broadcast_command(client: Client, message: Message):
+# Handler for video compression command
 
-    # Check if the message is sent by an authorized user
+@app.on_message(filters.video)
 
-    if message.from_user.id == 1078407883:
+def compress_video(client: Client, message: Message):
 
-        # Get the message text
+    video = message.video
 
-        broadcast_message = get_text(message)  # Remove the "/broadcast " part from the message
+    if video:
 
-        # Send the broadcast message to each user
+        # Download the video file
 
-        for user_id in users:
+        video_file = f"{video.file_id}.mp4"
 
-            try:
-
-                client.send_message(chat_id=user_id, text=f"**Kannan**: {broadcast_message}")
-
-            except Exception as e:
-
-                print(f"Failed to send message to user {user_id}: {e}")
-
-        success_message = "Broadcast sent successfully!"
-
-        client.send_message(chat_id=message.chat.id, text=success_message)
-
-    else:
-
-        error_message = "Sorry, you don't have permission to use this command."
-
-        client.send_message(chat_id=message.chat.id, text=error_message)
-        
- # Function to handle /users command
-
-@app.on_message(filters.command("b"))
-
-def handle_users_command(client: Client, message: Message):
-
-    # Check if the message is sent by an authorized user
-
-    if message.from_user.id == 1078407883:
-
-        # Get the total number of users
-
-        total_users = len(users)
-
-        users_message = f"Total users: {total_users}"
-
-        client.send_message(chat_id=message.chat.id, text=users_message)
-
-    else:
-
-        error_message = "Sorry, you don't have permission to use this command."
-
-        client.send_message(chat_id=message.chat.id, text=error_message)
-        
-@app.on_message(filters.command("v"))
-def handle_broadcast_command(client: Client, message: Message):
-
-    # Check if the message is sent by an authorized user
-
-    if message.from_user.id == 5737877514:
-
-        # Get the message text
-
-        broadcast_message = get_text(message)  # Remove the "/broadcast " part from the message
-
-        # Send the broadcast message to each user
-
-        for user_id in users:
-
-            try:
-
-                client.send_message(chat_id=user_id, text=f"**Abhiraj**: {broadcast_message}")
-
-            except Exception as e:
-
-                print(f"Failed to send message to user {user_id}: {e}")
-
-        success_message = "Broadcast sent successfully!"
-
-        client.send_message(chat_id=message.chat.id, text=success_message)
-
-    else:
-
-        error_message = "Sorry, you don't have permission to use this command."
-
-        client.send_message(chat_id=message.chat.id, text=error_message)
-       
-@app.on_message(filters.command("ab"))
-def handle_broadcast_command(client: Client, message: Message):
-
-    # Check if the message is sent by an authorized user
-
-    if message.from_user.id == 1414132123:
-
-        # Get the message text
-
-        broadcast_message = message.text[11:]  # Remove the "/broadcast " part from the message
-
-        # Send the broadcast message to each user
-
-        for user_id in users:
-
-            try:
-
-                client.send_message(chat_id=user_id, text=f"**Abhishek**: {broadcast_message}")
-
-            except Exception as e:
-
-                print(f"Failed to send message to user {user_id}: {e}")
-
-        success_message = "Broadcast sent successfully!"
-
-        client.send_message(chat_id=message.chat.id, text=success_message)
-
-    else:
-
-        error_message = "Sorry, you don't have permission to use this command."
-
-        client.send_message(chat_id=message.chat.id, text=error_message)
+        video.download(file_name=video_file)
 
         
 
+        # Compress the video using ffmpeg
+
+        compressed_file = f"compressed_{video.file_id}.mp4"
+
+        os.system(f"ffmpeg -i {video_file} -vf 'scale=640:480' -c:a copy {compressed_file}")
+
         
-# Run the Pyrogram client
+
+        # Send the compressed video back to the user
+
+        client.send_video(
+
+            chat_id=message.chat.id,
+
+            video=compressed_file,
+
+            caption="Here is your compressed video."
+
+        )
+
+        
+
+        # Delete the temporary files
+
+        os.remove(video_file)
+
+        os.remove(compressed_file)
+
+    else:
+
+        client.send_message(
+
+            chat_id=message.chat.id,
+
+            text="Please send a video file to compress."
+
+        )
+
+# Start the bot
 
 app.run()
+        
